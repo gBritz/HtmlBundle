2a68447b1a08d6fb300343d0c112eeca528cc9b6
gBritz <gbritzv@gmail.com>
2015-10-19 01:44:56 -0200

    feat(#3): Remove all whitespace and tab space

---
+++ src/HtmlBundle.Test/CollapseWhitespaceCompilerTest.cs
@@ -0,0 +1,66 @@
+﻿using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace HtmlBundle.Test
+{
+    [TestClass]
+    public class CollapseWhitespaceCompilerTest
+    {
+        [TestMethod]
+        public void When_file_is_null_should_throw_argument_null_exception()
+        {
+            var compiler = new CollapseWhitespaceCompiler();
+            Action method = () => compiler.Compile(null);
+            method.ShouldThrow<ArgumentNullException>()
+                .And.ParamName.Should().Be("file");
+        }
+
+        [TestMethod]
+        public void Given_spaces_begin_tag_when_compile_result_should_not_contains_space()
+        {
+            var compiler = new CollapseWhitespaceCompiler();
+            var content = new FileContent { Content = "       <p>Oi</p>" };
+            compiler.Compile(content)
+                .Content.Should().Be("<p>Oi</p>");
+        }
+
+        [TestMethod]
+        public void Given_spaces_end_tag_when_compile_result_should_not_contains_space()
+        {
+            var compiler = new CollapseWhitespaceCompiler();
+            var content = new FileContent { Content = "<p>Oi</p>       " };
+            compiler.Compile(content)
+                .Content.Should().Be("<p>Oi</p>");
+        }
+
+        [TestMethod]
+        public void Given_spaces_tag_when_compile_result_should_contains_one_space()
+        {
+            var compiler = new CollapseWhitespaceCompiler();
+            var content = new FileContent { Content = "       <div class='wrap'>       <p>        <span>Oi</span>       </p>       </div>       " };
+            compiler.Compile(content)
+                .Content.Should().Be("<div class='wrap'> <p> <span>Oi</span> </p> </div>");
+        }
+
+        [TestMethod]
+        public void Given_multi_line_spaces_tag_when_compile_result_should_contains_one_space()
+        {
+            var compiler = new CollapseWhitespaceCompiler();
+            var content = new FileContent { Content = @"
+
+<div class='wrap'>
+<p>
+<span>Oi</span>
+
+</p>
+
+</div>
+
+
+" };
+            compiler.Compile(content)
+                .Content.Should().Be(@"<div class='wrap'> <p> <span>Oi</span> </p> </div>");
+        }
+    }
+}
---
+++ src/HtmlBundle/CollapseWhitespaceCompiler.cs
@@ -0,0 +1,18 @@
+﻿using System.Text.RegularExpressions;
+
+namespace HtmlBundle
+{
+    public class CollapseWhitespaceCompiler : ITaskCompiler
+    {
+        // positive look behind for ">", one or more whitespace (non-greedy), positive lookahead for "<"
+        private static readonly Regex whitespace = new Regex(@"(?<=>)\s+?(?=<)", RegexOptions.Singleline);
+
+        public FileContent Compile(FileContent file)
+        {
+            Checker.IsNull(file, "file");
+
+            file.Content = whitespace.Replace(file.Content.Trim(), " ");
+            return file;
+        }
+    }
+}
